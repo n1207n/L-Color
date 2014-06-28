db6dfe88c589b0e3c052cf5dcf3d51af942ec63f
Silin Na <n1207n@gmail.com>
2014-06-28 00:11:50 -0700

    Initial Project setup

---
+++ tv/src/main/java/silin/color/Main.java
@@ -0,0 +1,13 @@
+package silin.color;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
