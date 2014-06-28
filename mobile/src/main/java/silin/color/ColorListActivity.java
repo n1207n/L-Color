6ea92509fbdec150f294ee088f0ff977de5f4f76
Silin Na <n1207n@gmail.com>
2014-06-28 00:22:58 -0700

    Updated the gitignore for iml file. + Changed the string value for the activity label of ColorListActivity.

--- mobile/src/main/java/silin/color/ColorListActivity.java
+++ mobile/src/main/java/silin/color/ColorListActivity.java
@@ -19,6 +19,7 @@ public class ColorListActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_list);
+
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
